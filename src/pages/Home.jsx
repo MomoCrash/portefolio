37fe7a54f34ea56520a64bbbaee70e05aafd2812
MomoCrash <44437470+MomoCrash@users.noreply.github.com>
2025-02-10 12:36:57 +0100

    [Update] Links

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -179,7 +179,7 @@ export default function Home() {
 
                         <div className={"container in-row justify-start"}>
                             <iframe ref={ref} className={"project-image"} width="560" height="315"
-                                    src="https://www.youtube.com/embed/NSxrJB3lpgI?si=dv7hXxQDChQ8zmGi?&autoplay=1&mute=1"
+                                    src="https://www.youtube.com/embed/NSxrJB3lpgI?si=dv7hXxQDChQ8zmGi?&autoplay=1&mute=1&loop=1"
                                     title="YouTube video player" frameBorder="0"
                                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                                     referrerPolicy="strict-origin-when-cross-origin" allowFullScreen>
@@ -205,7 +205,7 @@ export default function Home() {
 
                         <div className={"container in-row justify-start"}>
                             <iframe ref={ref} className={"project-image"} width="560" height="315"
-                                    src="https://www.youtube.com/embed/7ZslUnYMS2E?si=dv7hXxQDChQ8zmGi?&autoplay=1&volume=0.1"
+                                    src="https://www.youtube.com/embed/7ZslUnYMS2E?si=dv7hXxQDChQ8zmGi?&autoplay=1&mute=1&loop=1"
                                     title="YouTube video player" frameBorder="0"
                                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                                     referrerPolicy="strict-origin-when-cross-origin" allowFullScreen>
